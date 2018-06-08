d40dcd46e405ba988b3a12ba0c6268a94f43484f
OwenZhu <398094982@qq.com>
2018-06-08 12:08:28 +1000

    upload 2

--- EASY/1_Two_Sum.py
+++ EASY/1_Two_Sum.py
@@ -17,6 +17,11 @@ return [0, 1].
 
 '''
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution:
     def twoSum(self, nums, target):
